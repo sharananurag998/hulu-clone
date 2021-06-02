8f83a9d07e05e2c4d6191969193d6873bde6291f
Anurag Sharan <sharananurag998@gmail.com>
2021-06-02 12:55:05 +0530

    Added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {animation: ['hover', 'focus', 'group-hover']},
   },
   plugins: [],
 }
